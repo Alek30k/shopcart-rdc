efe972236324a97aa9e3be2acbd09d6fedd549e9
Alek30k <alemank23k@gmail.com>
2025-06-03 22:34:36 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div className="p-10 bg-shop_light_pink">
@@ -8,6 +10,7 @@ const Home = () => {
         libero error atque molestiae ducimus labore vitae adipisci quisquam!
         Dolore, aut ullam!
       </p>
+      <Button size="lg">holaaaaaaaaaaaaa</Button>
     </div>
   );
 };
