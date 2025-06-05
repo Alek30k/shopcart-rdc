fd5cc4f0a04f65d7f223380c593b5a98e4d87d26
Alek30k <alemank23k@gmail.com>
2025-06-04 21:58:41 -0300

    last commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,17 @@
 import Container from "./Container";
+import HeaderMenu from "./HeaderMenu";
 import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className=" bg-white py-5">
-      <Container>
+    <header className=" bg-white py-5 border-b border-b-black/20">
+      <Container className="flex items-center justify-between">
         {/* Logo */}
         <Logo />
         {/* NavBotton */}
+        <HeaderMenu />
         {/* NavAdmin */}
-        Headerrr
+        <div className="">Others</div>
       </Container>
     </header>
   );
