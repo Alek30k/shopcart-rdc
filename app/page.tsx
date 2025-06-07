748b83c6887fcffc7891f2ad2ddb7a0e2ea03ec2
Alek30k <alemank23k@gmail.com>
2025-06-07 16:04:51 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import Container from "@/components/Container";
-import { Button } from "@/components/ui/button";
+import HomeBanner from "@/components/HomeBanner";
 
 const Home = () => {
   return (
     <Container className="bg-shop_light_pink">
-      <h2 className="text-xl font-semibold">Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-        perspiciatis nobis fuga repellat, ipsum, consequuntur tempore ipsam et
-        libero error atque molestiae ducimus labore vitae adipisci quisquam!
-        Dolore, aut ullam!
-      </p>
-      <Button size="lg">holaaaaaaaaaaaaa</Button>
+      <HomeBanner />
     </Container>
   );
 };
