40c5b22010a749e9271b1103d1dacd86016114a9
Alek30k <alemank23k@gmail.com>
2025-06-07 15:03:00 -0300

    last commit

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -2,7 +2,7 @@ import { SignInButton } from "@clerk/nextjs";
 
 const SignIn = () => {
   return (
-    <SignInButton>
+    <SignInButton mode="modal">
       <button className="text-sm font-semibold hover:text-darkcolor text-lightcolor hover:cursor-pointer hoverEffect">
         Login
       </button>
