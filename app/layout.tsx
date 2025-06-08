32e6072357fd6ed5d9e692c3f8de640176e86e9e
Alek30k <alemank23k@gmail.com>
2025-06-07 22:38:19 -0300

    last commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,21 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-
-export const metadata: Metadata = {
-  title: "RDC",
-  description: "Welcome to ShopCart Regalos del Corazón",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+// import { Toaster } from "react-hot-toast";
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex min-h-screen flex-col ">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="font-poppins antialiased">
+        {children}
+        {/* <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: "#000000",
+              color: "#fff",
+            },
+          }}
+        /> */}
+      </body>
+    </html>
   );
-}
+};
+export default RootLayout;
