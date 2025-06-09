75f0325fd35fb2ef36d09f7e8d4a23b7d97abb7d
Alek30k <alemank23k@gmail.com>
2025-06-08 22:00:59 -0300

    last commit

--- components/PriceView.tsx
+++ components/PriceView.tsx
@@ -7,6 +7,7 @@ interface Props {
   discount: number | undefined;
   className?: string;
 }
+
 const PriceView = ({ price, discount, className }: Props) => {
   return (
     <div className="flex items-center justify-between gap-5">
@@ -15,14 +16,26 @@ const PriceView = ({ price, discount, className }: Props) => {
           amount={price}
           className={cn("text-shop_dark_green", className)}
         />
-        {price && discount && (
-          <PriceFormatter
-            amount={price + (discount * price) / 100}
-            className={twMerge(
-              "line-through text-xs font-normal text-zinc-500",
-              className
+        {price && discount !== undefined && (
+          <>
+            {discount > 0 && (
+              <PriceFormatter
+                amount={price + (discount * price) / 100}
+                className={twMerge(
+                  "line-through text-xs font-normal text-zinc-500",
+                  className
+                )}
+              />
             )}
-          />
+            {discount === 0 && (
+              <span
+                className={twMerge(
+                  "text-xs font-normal text-zinc-500",
+                  className
+                )}
+              ></span>
+            )}
+          </>
         )}
       </div>
     </div>
